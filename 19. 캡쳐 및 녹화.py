a59f1a1c298e913f413d1d8d332f535899e71f88
taki0412 <taki041210@naver.com>
2021-05-04 14:42:02 +0900

    19. 캡처 및 녹화

---
+++ 19. 캡쳐 및 녹화.py
@@ -0,0 +1,52 @@
+# Import Library
+import datetime
+import cv2
+
+# 비디오 경로 설정 함수
+cap = cv2.VideoCapture("image/3.mp4")
+# 디지털 미디오 포맷 코드 설정
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# 녹화 유/무 설정
+record = False
+
+while True:
+    if (cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT)):
+        cap.open("image/3.mp4")
+
+    # ret : 카메라 작동 유/무
+    # frame = 이미지 읽어 저장하기
+    ret, frame = cap.read()
+    cv2.imshow("VideoFrame",frame)
+
+    # now에 파일의 제목을 설정함 : 날짜_시간-분-초 형식
+    now = datetime.datetime.now().strftime("%d_%H-%M-%S")
+    # 4ms 마다 키보드 키의 값이 저장된다.
+    key = cv2.waitKey(4)
+
+    # 3 = ESC, 2.5 = CNTL + Z, 2 = CTRL + X, 1 = CTRL + C를 의미한다.
+    # ESC : 프로그램 종류
+    # CTRL + Z : 현재 화면을 캡처 한다.
+    # CTRL + X : 녹화를 시작한다.
+    # CTRL + C : 녹화를 중지한다.
+    if key == 3:
+        break
+    elif key == 2.5:
+        print('캡쳐')
+        cv2.imwrite("image/" + str(now) + ".png", frame)
+    elif key == 2:
+        print("녹화 시작")
+        record = True
+        video = cv2.VideoWritter("image/" + str(now) + ".avi", fourcc, 20.0, (frame.shape[1], frame.shape[0]))
+    elif key == 1:
+        print("녹화 중지")
+        record = False
+        video.release()
+
+    if record == True:
+        print("녹화 중..")
+        video.write(frame)
+
+
+cap.release()
+# 모든 윈도우 창 제거 함수
+cv2.destroyAllWindows()
