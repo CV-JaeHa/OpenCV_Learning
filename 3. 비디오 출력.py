fd0260dba22333cfb180c5f385c0390b0cdcb207
taki0412 <taki041210@naver.com>
2021-06-11 15:02:41 +0900

    3. 비디오 출력

--- 3. 비디오 출력.py
+++ 3. 비디오 출력.py
@@ -4,8 +4,18 @@ import cv2
 # 동영상 파일 설정
 cap = cv2.VideoCapture("image/3.mp4")
 
-# 33초 동안 반복
-while cv2.waitKey(33) < 0:
+# 6초 동안 반복
+while cv2.waitKey(6) < 0:
     # 현재 동영상의 프레임 수와 동영상의 총 프레임 수를 받아온다.
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_POS_FRAMES):
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    # read() 카메라의 상태와 프레임을 받아온다.
+    ret, frame = cap.read()
+    # 이미지 출력함수
+    cv2.imshow("VideoFrame", frame)
+
+# 메모리 해제
+cap.release()
+# 모든 윈도우 창 제거
+cv2.destroyAllWindows()
