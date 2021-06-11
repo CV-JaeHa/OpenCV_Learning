5cbfdc064cc1bf90a75c39fc5e3b56e357c10df0
taki0412 <taki041210@naver.com>
2021-06-11 15:04:44 +0900

    9. 색상 공간 변환

--- 9. 색상 공간 변환.py
+++ 9. 색상 공간 변환.py
@@ -4,11 +4,11 @@ import cv2
 # 이미지 경로 설정
 src = cv2.imread("image/9.jpg", cv2.IMREAD_COLOR)
 # 이미지 색상 공간 변경
-# cv2.cvtColor(src, code, dstCn) : 입력 이미지(src), 색상변환코드(code), 출력채널(dstCn)으로 출력이미지(dst)를 생성합니다.
+# cv2.cvtColor(src, code, dstCn) : 입력 이미지(src), 색상변환코드(code), 출력채널(dstCn)으로
+# 출력이미지(dst)를 생성합니다.
 # 색상 변환 코드는 원본 이미지, 색상 공간 결과 이미지 색상 공간을 의미합니다.
 # 원본 이미지 색상 공간은 원본 이미지와 일치해야 합니다.
 dst = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-
 # 이미지 출력 함수
 cv2.imshow("src", src)
 cv2.imshow("dst", dst)
